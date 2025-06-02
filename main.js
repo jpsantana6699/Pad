06384b08e101941d8acb2864308460fccca5f97d
João Pedro Santana <jpsantana003@gmail.com>
2025-06-02 17:29:51 -0300

    feat:pads

---
+++ main.js
@@ -0,0 +1,54 @@
+function tocaSomJack() {
+  document.querySelector('#som_tecla_Jack').play()
+}
+
+document.querySelector('.tecla_Jack').onclick = tocaSomJack;
+
+function tocaSomGay() {
+  document.querySelector('#som_tecla_Gay').play()
+}
+
+document.querySelector('.tecla_gay').onclick = tocaSomGay;
+
+function tocaSomDom() {
+  document.querySelector('#som_tecla_Dom').play()
+}
+
+document.querySelector('.tecla_Dom').onclick = tocaSomDom;
+
+function tocaSomOruan() {
+  document.querySelector('#som_tecla_Oruan').play()
+}
+
+document.querySelector('.tecla_Oruan').onclick = tocaSomOruan;
+
+function tocaSomChavin() {
+  document.querySelector('#som_tecla_Chavin').play()
+}
+
+document.querySelector('.tecla_Chavin').onclick = tocaSomChavin;
+
+function tocaSomMorango() {
+  document.querySelector('#som_tecla_Morango').play()
+}
+
+document.querySelector('.tecla_Morango').onclick = tocaSomMorango;
+
+function tocaSomCebolinha() {
+  document.querySelector('#som_tecla_Cebolinha').play()
+}
+
+document.querySelector('.tecla_Cebolinha').onclick = tocaSomCebolinha;
+
+function tocaSomTung() {
+  document.querySelector('#som_tecla_Tung').play()
+}
+
+document.querySelector('.tecla_Tung').onclick = tocaSomTung;
+
+
+function tocaSomGrosso() {
+  document.querySelector('#som_tecla_Grosso').play()
+}
+
+document.querySelector('.tecla_Grosso').onclick = tocaSomGrosso;
